60fc2a6d3b43f7cd5b4236e0e6fc50c96bd603bf
faisolp <faisolp@gmail.com>
2022-12-01 19:46:57 +0700

    test get by id

--- src/test01.ts
+++ src/test01.ts
@@ -1,8 +1,11 @@
 import JournalStockItemMongoController from "./controllers/JournalStockItem.mongo.controller"
-
- 
+import { Types } from "mongoose";
+const {ObjectId}  =Types 
 
 export default async () =>{
-  let data =await (new JournalStockItemMongoController()).get();
+  let data =await (new JournalStockItemMongoController()).get({_id:{$gt:new ObjectId("6383194df86bd71ecd947ff1")}},{},{},10);
+  console.log('====================================');
+  console.log(" data", data);
+  console.log('====================================');
 
 }
